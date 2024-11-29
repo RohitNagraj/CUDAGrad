Fixed the bug with gradients not propogating due to list of neurons.

cudagrad/neuron.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 
 from cudagrad.tensor import Tensor1D
@@ -5,12 +7,19 @@ from cudagrad.tensor import Tensor1D
 
 class Neuron:
     def __init__(self, n_input):
-        self.w = Tensor1D(np.random.randn(n_input))
-        self.b = Tensor1D(np.random.randn(1))
+        self.backend = 'cuda'
+        self.n_input = n_input
+        self.w = Tensor1D(np.random.randn(n_input), backend=self.backend)
+        self.b = Tensor1D(np.random.randn(1), backend=self.backend)
+
+    def __repr__(self):
+        return f"Neuron(n_input={self.n_input})"
 
     def __call__(self, x: Tensor1D):
+        start = time.time()
         activation = self.w.dot(x) + self.b
         output = self._tanh(activation)
+        print(f"Time taken per neuron: {time.time() - start}")
         return output
 
     def _tanh(self, x: Tensor1D):